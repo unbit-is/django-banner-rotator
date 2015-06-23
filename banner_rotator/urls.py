1e661034cb3f59f12ca40d4a67baa7d0332bc0c9
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2015-06-23 16:07:39 +0200

    Add view to render a banner given a place
    If no banner is found a 404 error is returned so it's easy to
    catch in js.

--- banner_rotator/urls.py
+++ banner_rotator/urls.py
@@ -9,4 +9,5 @@ except ImportError:
 
 urlpatterns = patterns('banner_rotator.views',
     url(r'^click/(?P<banner_id>\d{1,4})/$', 'click', name='banner_click'),
+    url(r'^banner/place/(?P<place_slug>[\-\w]+)/$', 'banner_per_place', name='banner_per_place'),
 )
--- banner_rotator/views.py
+++ banner_rotator/views.py
@@ -1,8 +1,9 @@
 #-*- coding:utf-8 -*-
 
-from django.shortcuts import redirect, get_object_or_404
+from django.http import Http404
+from django.shortcuts import redirect, get_object_or_404, render
 
-from banner_rotator.models import Banner
+from banner_rotator.models import Banner, Place
 
 
 def click(request, banner_id):
@@ -10,3 +11,19 @@ def click(request, banner_id):
     banner.click(request)
 
     return redirect(banner.url)
+
+def banner_per_place(request, place_slug):
+    place = get_object_or_404(Place, slug=place_slug)
+
+    try:
+        banner_obj = Banner.objects.biased_choice(place)
+        banner_obj.view()
+    except Banner.DoesNotExist:
+        raise Http404
+
+    print banner_obj, type(banner_obj), place
+    ctx = {
+        'banner': banner_obj,
+        'banner_place': place,
+    }
+    return render(request, 'banner_rotator/place.html', ctx)
